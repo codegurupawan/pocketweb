8b416cb1c465b245e7a00762ca34d2def495c935
pk050101 <pk.it050101@gmail.com>
2023-01-24 23:48:21 +0530

    Welcome Page has been Designed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,18 @@ function App() {
   return (
     <div className="App">
 
-      
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+
+          <Route path="/" element={<Welcome />} />
+          <Route path='/home' element={<Home />} />
+        </Routes>
+
+      </BrowserRouter>
+
 
     </div>
   );
